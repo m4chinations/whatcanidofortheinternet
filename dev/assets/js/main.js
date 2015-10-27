48116b52b256977a07352625ed40ac3c4ec0d891
Tennyson Holloway <tennyson.holloway@gmail.com>
2015-10-27 11:37:32 -0400

    comments

--- dev/assets/js/main.js
+++ dev/assets/js/main.js
@@ -1,36 +1,37 @@
 var element = document.getElementById('editor');
 
 
+/* create the action item editor in #editor */
 var editor = new JSONEditor(element, {
     theme : 'bootstrap3',
-    startval : actionItemArray,
+    startval : actionItemArray, //this is just an array with data [ { heading: 'eg', blurb: ...
     schema : {
-        type : 'array',
-        title : 'Action Items',
-        format : 'tabs',
-        options : {
+        type : 'array', //we are editing an array of objects
+        title : 'Action Items', 
+        format : 'tabs', //show the editor with tabs
+        options : { //disable some editor options that are not used
             disable_array_reorder : true,
             collapsed : true,
         },
         items : {
-            type : 'object',
+            type : 'object', //the items in the array are objects 
             title : 'Action Item',
-            headerTemplate: "{{self.heading}}",
-            options : {
+            headerTemplate: "{{self.heading}}", //puts tab heading as the element's heading
+            options : { //disable some editor options that are not used
                 disable_array_reorder : true,
                 disable_collapse : true,
                 disable_edit_json : true,
                 disable_properties : true,
             },
-            properties : {
-                heading : {
+            properties : { //these are the properties of the objects in the array
+                heading : { //heading is a string value
                     type : 'string'
                 }, 
-                blurb : {
+                blurb : { //the blurb is also a string, but its bigger, so we give it a textarea format
                     type : 'string',
                     format : 'textarea',
                 },
-                link : {
+                link : { //the link is an object, so we just repeat the same structure as items : { } found above
                     type : 'object',
                     properties : {
                         url : {
@@ -49,6 +50,9 @@ var editor = new JSONEditor(element, {
 });
 
 var orgElement = document.getElementById('orgeditor');
+
+
+/* creating another editor for the organizations, very similar to above */
 var orgEditor = new JSONEditor(orgElement, {
     theme : 'bootstrap3',
     startval : organizationArray,
@@ -86,15 +90,19 @@ var orgEditor = new JSONEditor(orgElement, {
     }
 });
 
+/* click handler */
 document.getElementById('json').addEventListener('click',function() {
     // Get the value from the editor
     var actionJson = editor.getValue();
     var orgJson = orgEditor.getValue();
+    //append them together in one array
     var json = {
         'actionjson' : actionJson,
         'orgjson' : orgJson
     };
+    //log it to the console
     console.log(json);
+    //and also append it to the body of the document.
     if (document.getElementById('code')) {
         document.getElementById('code').textContent = JSON.stringify(json);
     } else {
